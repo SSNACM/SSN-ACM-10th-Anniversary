d2e7ab1c604450cbdfe52d9b842284982c2dac69
Mitul Krishna <60043776+Mitul9703@users.noreply.github.com>
2023-04-11 17:39:07 +0530

    Updated with rotate shift keying

--- Caesar-ECC Hybrid Encryption for ResourceLimited Devices.py
+++ Caesar-ECC Hybrid Encryption for ResourceLimited Devices.py
@@ -1,9 +1,10 @@
 import ecdsa as e
 import ecdsa.util as eu
 import secrets
+import timeit
 
-def generate_key():
-    return secrets.randbelow(26)
+def generate_key(length):
+    return secrets.randbelow(length)
 
 def encryption(message):
     global k
@@ -13,23 +14,25 @@ def encryption(message):
     k = secrets.randbelow(order_of_curve-1) + 1
     public_key = g.x() * k
     secret_key = k * public_key
-    
-    otp = [secrets.randbelow(26) for _ in range(len(message))] # generate one-time pad
-    keys = [generate_key() for _ in range(len(message))] # generate random keys
-    
+
+    otp = [secrets.randbelow(len(message)) for _ in range(len(message))] # generate one-time pad
+    keys = [generate_key(len(message)) for _ in range(len(message))] # generate random keys
+
     encrypt_msg = ""
     for i, char in enumerate(message):
         shift = (keys[i % len(keys)] + otp[i]) % 26 # add one-time pad to shift
         encrypt_msg += caesar_cipher(char, shift)
-    
+
+    print("Encrypted message using rotating shift keying caesar cipher and one time pad, before converting it to point :", encrypt_msg)
+
     point = 0
     for char in encrypt_msg:
         point = point * 1000 + ord(char)
-    
+
     sp = public_key*secret_key
     sg = secret_key*g.x()
     encrypted_point = point + sp
-    
+
     return (sg,encrypted_point, otp, keys) # return one-time pad and keys with encrypted message
 
 def decryption(encrypted_msg, otp, keys):
@@ -41,21 +44,21 @@ def decryption(encrypted_msg, otp, keys):
     while encrypted_point > 0:
         real_msg = chr(encrypted_point % 1000) + real_msg  
         encrypted_point = encrypted_point // 1000  
-    
+
     decrypt_msg = ""
     for i, char in enumerate(real_msg):
         shift = (keys[i % len(keys)] + otp[i]) % 26 # add one-time pad to shift
         decrypt_msg += caesar_cipher(char, -shift) # use negative shift to decrypt
-    
+
     return decrypt_msg
 
 def caesar_cipher(char, key):
-    if char.isalpha():
-        shift = key % 26
-        char_num = ord(char.lower()) - ord('a')
-        shifted_num = (char_num + shift) % 26
-        shifted_char = chr(shifted_num + ord('a'))
-        return shifted_char.upper() if char.isupper() else shifted_char
+    if 32 <= ord(char) <= 126:  
+        shift = key % 95  
+        char_num = ord(char) - 32  
+        shifted_num = (char_num + shift) % 95
+        shifted_char = chr(shifted_num + 32)  
+        return shifted_char
     else:
         return char
 
@@ -63,6 +66,8 @@ def caesar_cipher(char, key):
 if __name__ == "__main__":
     message = "!-- $ Hello 1234 -- !#" 
     ency = encryption(message)
+    time_taken = timeit.timeit(lambda: encryption(message), number=100)
+    print(time_taken)
     print("Encrypted message:", ency[1])
     print("One-time pad:", ency[2])
     print("Keys:", ency[3])
